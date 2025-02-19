91e4527f37a8d327113cc14fb8b9796f002fc74a
FelipeArnus <79066040+FelipeArnus@users.noreply.github.com>
2025-02-19 11:58:38 -0300

    Add files via upload

---
+++ Aula/Aula/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Specialized;
+
+class Program
+{
+    static void Main()
+    {
+
+        DateTime hoje = DateTime.Now; //data de hj
+
+
+        Console.Write("Diga sua data de nascimento():");
+        string nascimento = Console.ReadLine();
+        DateTime nsc = DateTime.Parse(nascimento);
+        Console.Write($"Sua idade é {hoje.Year - nsc.Year}");
+
+
+    } 
+
+
+}
